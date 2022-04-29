6e4868dc66b1d67e42ba800c5cb2c20688f662e9
cribemont <charles.ribemont@gmail.com>
2022-04-29 11:18:05 +0200

    MAC changer comments

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -7,11 +7,17 @@ def get_arguments():
     parser = optparse.OptionParser
     parser.add_option("-i", "--interface", dest="interface", help="Value of the interface you want to change")
     parser.add_option("-m", "--mac", dest="destination_mac_address", help="Value of the new MAC")
+    # parse arguments passed to the command line
     (options, arguments) = parser.parse_args()
+    # do we have an interface ?
     if not options.interface:
+        # : display error
         parser.error("[-] Please specify an interface with -i or --interface")
+    # do we have a new MAC ?
     elif not options.destination_mac_address:
+        # : display error
         parser.error("[-] Please specify a MAC address with -m or --mac")
+    # proceed
     return options
 
 # main function to update MAC address
@@ -21,6 +27,7 @@ def change_mac_address(interface, destination_mac_address):
     subprocess.call(["ifconfig", interface, "hw", "ether", destination_mac_address])
     subprocess.call(["ifconfig", interface, "up"])
 
-
+# check and get options
 options = get_arguments()
+# proceed with updating the MAC
 change_mac_address(options.interface, options.destination_mac_address)
